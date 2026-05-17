8d6b11f23bf0c588792a94446469eb021062e9a2
MusangQim <musangqim03@gmail.com>
2026-05-17 21:10:06 +0800

    update: add import potion.py  and assign healing_potion to heal() for Part II

--- alchemy/__init__.py
+++ alchemy/__init__.py
@@ -1,3 +1,5 @@
 from alchemy.elements import create_air
-# Just testing if adding earth"
-# from alchemy.elements import create_earth
+from alchemy.potions import strength_potion, healing_potion
+
+
+heal = healing_potion
