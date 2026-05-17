a4a2b5a5db9259e474926147fc73b392acef2c87
MusangQim <musangqim03@gmail.com>
2026-05-17 20:39:14 +0800

    potion added

---
+++ alchemy/potions.py
@@ -0,0 +1,12 @@
+from alchemy.elements import create_earth, create_air
+from elements import create_fire, create_water
+
+
+def healing_potion():
+    return (f"Healing potion brewed with"
+            f" '{create_earth()}' and '{create_air()}'")
+
+
+def strength_potion():
+    return (f"Strength potion brewed with"
+            f" '{create_fire()}' and '{create_water()}'")
