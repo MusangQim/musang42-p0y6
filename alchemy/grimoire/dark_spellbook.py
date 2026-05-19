1b2616d1c1bf85d2da1bccdf687ba455802a4813
MusangQim <musangqim03@gmail.com>
2026-05-19 22:29:34 +0800

    update: similar to light_spellbook but change to 4 elements for dark things

--- alchemy/grimoire/dark_spellbook.py
+++ alchemy/grimoire/dark_spellbook.py
@@ -1,3 +1,10 @@
+from alchemy.grimoire.dark_validator import validate_ingredients
+
+
 def dark_spell_allowed_ingredients():
+    return ["bats", "frogs", "arsenic", "eyeball"]
+
 
 def dark_spell_record(spell_name: str, ingredients: str):
+    dark_status = validate_ingredients(ingredients)
+    return (f"Spell recorded: {spell_name} ({dark_status})")
