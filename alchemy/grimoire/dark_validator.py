b7eea27bd3b6a490fbe38dbf0f6a0880f44d00d8
MusangQim <musangqim03@gmail.com>
2026-05-19 22:30:20 +0800

    update: similar to light validator

--- alchemy/grimoire/dark_validator.py
+++ alchemy/grimoire/dark_validator.py
@@ -1,1 +1,9 @@
+from alchemy.grimoire.dark_spellbook import dark_spell_allowed_ingredients
+
+
 def validate_ingredients(ingredients: str):
+    allowed = dark_spell_allowed_ingredients()
+    lowcase = ingredients.lower()
+    if any(item in lowcase for item in allowed):
+        return (f"ingredients [{ingredients}]: VALID")
+    return (f"ingredients [{ingredients}]: INVALID")
