d7de16b22bb10fbbf7623dd09a60546b4828888d
MusangQim <musangqim03@gmail.com>
2026-05-19 21:28:34 +0800

    update: add import lead_to_gold from recipes

--- alchemy/__init__.py
+++ alchemy/__init__.py
@@ -1,5 +1,5 @@
 from alchemy.elements import create_air
 from alchemy.potions import strength_potion, healing_potion
-
+from alchemy.transmutation.recipes import lead_to_gold
 
 heal = healing_potion
