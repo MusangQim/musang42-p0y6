107e30be6762c3a32f0e14c9c08c7f6349e26449
MusangQim <musangqim03@gmail.com>
2026-05-20 21:10:07 +0800

    update: remove any import to avoid circular dependency

--- alchemy/grimoire/light_validator.py
+++ alchemy/grimoire/light_validator.py
@@ -1,9 +1,6 @@
-from alchemy.grimoire.light_spellbook import light_spell_allowed_ingredients
-
-
 def validate_ingredients(ingredients: str):
-    allowed = light_spell_allowed_ingredients()
+    allowed = ["earth", "air", "fire", "water"]
     lowcase = ingredients.lower()
     if any(item in lowcase for item in allowed):
-        return (f"ingredients [{ingredients}]: VALID")
-    return (f"ingredients [{ingredients}]: INVALID")
+        return (f"{ingredients} - VALID")
+    return (f"{ingredients} - INVALID")
